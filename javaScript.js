1ed73051063bfa899d02a46643d50a7d022475b1
Alinka08 <alinazoldubaeva@gmail.com>
2023-05-17 16:27:38 -0400

    first commit

---
+++ javaScript.js
@@ -0,0 +1,10 @@
+const n = () => {
+  if (n / 3 && n / 5) console.log("FizzBuzz");
+  else if (n / 3) {
+    console.log("Fizz");
+  } else if (n / 5) {
+    console.log("Buzz");
+  } else if (n != true) {
+    console.log("n");
+  }
+};
---
+++ zooAnimals.js
@@ -0,0 +1,13 @@
+const zooLion = "Alex";
+const zooZebra = "Marty";
+console.log(zooLion, zooZebra);
+console.log("A lion's name is: " + zooLion);
+console.log("A zebra's name is: " + zooZebra);
+
+var lionSound = "ROAAAARRR";
+const zebraSound = "HEHEHEHE";
+console.log(`${zooLion} says ${lionSound}`);
+console.log(`${zooZebra} says ${zebraSound}`);
+
+lionSound = "MEAAAOOOW";
+console.log(`${zooZebra} now says ${lionSound}`);
