57fb73b405b3a2f04797df582a26ae9ab5486c11
Alinka08 <alinazoldubaeva@gmail.com>
2023-05-17 21:47:28 -0400

    practiceOperators

---
+++ homework1.js
@@ -0,0 +1,35 @@
+// task 1
+var superhero = "Wonder Woman";
+var score = 5;
+console.log(superhero, "has mid-level skills:", score > 0 && score < 10);
+
+// task 2
+const damageReceived = 0;
+const battleTimeLeft = 0;
+const energy = 10;
+console.log("Game over:", battleTimeLeft == 0 || energy > damageReceived);
+
+// task 3
+const num1 = 2;
+const num2 = 5;
+const test1 = num1 % 2;
+const test2 = num2 % 2;
+const result1 = test1 == 0;
+const result2 = test2 == 0;
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
+
+// task 4
+console.log(100 + 10);
+
+// task 5
+const me = "I know ";
+const three = 3;
+const proLang = " programming language.";
+console.log(me + three + proLang);
+
+// task 6
+var mySkills = 0;
+mySkills += 5;
+mySkills += 15;
+console.log(mySkills);
